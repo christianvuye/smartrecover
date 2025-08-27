fb298572cc927d25f46ed8479ae557e067a95c80
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2025-08-27 10:24:42 +0200

    feat(partner_sync_service): scaffold app and register in settings

---
+++ partner_sync_service/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PartnerSyncServiceConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "partner_sync_service"
--- smartrecover/settings.py
+++ smartrecover/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "risk_service",
+    "partner_sync_service",
 ]
 
 MIDDLEWARE = [
